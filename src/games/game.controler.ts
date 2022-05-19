37e56586c983463317a16af14734d9e4243c3c7b
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-19 17:58:47 -0300

    feat(games) add endpoint create

--- src/games/game.controler.ts
+++ src/games/game.controler.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 
 @Controller('games')
 export class GameController{
   @Get()
   findAll(){
     return 'Buscar todos os games'
+  }
+
+  @Post()
+  create(){
+    return `criar um game`;
   }
 }
