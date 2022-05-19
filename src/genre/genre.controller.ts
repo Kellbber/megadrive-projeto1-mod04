56faf5db824ad2e047ff655119f9e1349f59ac18
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-19 19:18:55 -0300

    feat(genre): add endpoint create

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('genres')
 export class GenreController {
-
   @Get()
-  findAll(){
-    return `Buscar todos os gêneros`
+  findAll() {
+    return `Buscar todos os gêneros`;
   }
 
+  @Post()
+  create() {
+    return `Criar um gênero`;
+  }
 }
