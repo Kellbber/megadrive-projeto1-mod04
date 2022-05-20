bccbfb05b65ca3b7ab9543c1dc66ecfbd85150ab
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-20 17:54:16 -0300

    feat schemas game/genre and gitignore

--- src/games/dto/create-game.dto.ts
+++ src/games/dto/create-game.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString} from 'class-validator';
 
 export class CreateGameDto {
   @ApiProperty({ description: 'nome do game', example: 'Yu-gi-Oh' })
+  @IsString()
   name: string;
   @ApiProperty({ description: 'preço do game', example: '15' })
+  @IsNumber()
   price: number;
+  @IsString()
   @ApiProperty({
     description: 'descrição do game',
     example: 'Jogo desenvolvido por N e Y',
--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateGenreDto {
   @ApiProperty({ description: ' gênero do game', example: 'Ação' })
-  
+  @IsString()
   genre: string;
 }
