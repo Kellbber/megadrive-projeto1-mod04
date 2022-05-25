eb0cfd0e22c103d4e067ede4014475654961d03c
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-25 14:59:47 -0300

    feat(user): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { GameModule } from './games/game.module';
 import { GenreModule } from './genre/genre.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfileModule } from './profile/profile.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [GameModule, GenreModule, PrismaModule, ProfileModule],
+  imports: [GameModule, GenreModule, PrismaModule, ProfileModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
