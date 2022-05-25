5601cf4cff29420c79775ddcea717e867e7d98f7
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-25 14:40:37 -0300

    fix(profile): import prisma module on profile mod

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class CreateProfileDto {
   id: string;
+  @IsString()
+  @ApiProperty({
+    description: 'nome do perfil',
+    example: 'Kellbber'
+  })
   name: string;
 }
